46761d1d88836c8c3186789aca0158a19f99050e
Georgekuprashvili <kuprashviligeorge@gmail.com>
2025-01-28 15:58:54 +0400

    .

--- src/components/__atoms/api.jsx
+++ src/components/__atoms/api.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 export async function FetchData(setData) {
   try {
     const response = await axios.get("https://dummyjson.com/quotes");
+    setData(products.data);
   } catch (error) {
     console.log(error);
   }
