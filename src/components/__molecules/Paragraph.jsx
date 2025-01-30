412ad5f535a730a3e73706bd03ab37757271b8f8
Georgekuprashvili <kuprashviligeorge@gmail.com>
2025-01-30 20:39:37 +0400

    .

--- src/components/__molecules/Paragraph.jsx
+++ src/components/__molecules/Paragraph.jsx
@@ -14,7 +14,10 @@ function Paragraph() {
   return (
     <>
       <div className="paragraph_cont">
-        <h1 className="heading">FAQ</h1>
+        <div className="heading_box">
+          <h1 className="heading">FAQ</h1>
+        </div>
+
         <div className="ul_cont">
           <ul className="text_list">
             {data.slice(0, 5).map((product, key) => (
